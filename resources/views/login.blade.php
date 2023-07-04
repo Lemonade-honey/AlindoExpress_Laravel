58cf9959e324ffc5b1130b9e25491d9a7b0987eb
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-07-04 20:13:06 +0700

    Perubahan dan Penambahan desain UI serta penggabungan dengan backend

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,43 +1,113 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="./assets/css/main/style.css" />
     <title>Login</title>
-</head>
-<body>
-    <div class="container py-5">
-        <div class="w-50 center border rounded px-3 py-3 mx-auto">
-        <h1>Login</h1>
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+  </head>
+  <body>
+    <!----------------------- Main Container -------------------------->
+
+    <div
+      class="container d-flex justify-content-center align-items-center min-vh-100"
+    >
+      <!----------------------- Login Container -------------------------->
+
+      <div class="row border rounded-5 p-3 bg-white shadow box-area">
+        <!--------------------------- Left Box ----------------------------->
+
+        <div
+          class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box"
+          style="background: #103cbe"
+        >
+          <div class="featured-image mb-3">
+            <img
+              src="./assets/images/login-register/1.png"
+              class="img-fluid"
+              style="width: 250px"
+            />
+          </div>
+          <p
+            class="text-white fs-2"
+            style="
+              font-family: 'Courier New', Courier, monospace;
+              font-weight: 600;
+            "
+          >
+            Be Verified
+          </p>
+          <small
+            class="text-white text-wrap text-center"
+            style="width: 17rem; font-family: 'Courier New', Courier, monospace"
+            >Join experienced on this platform.</small
+          >
+        </div>
+
+        <!-------------------- ------ Right Box ---------------------------->
+
+        <div class="col-md-6 right-box">
+          <div class="row align-items-center">
+            <div class="header-text mb-4">
+              <h2>Hello,Again</h2>
+              <p>We are happy to have you back.</p>
+            @if ($errors->any())
+              <div class="alert alert-danger">
+                  <ul>
+                      @foreach ($errors->all() as $error)
+                          <li>{{ $error }}</li>
+                      @endforeach
+                  </ul>
+              </div>
+            @endif
             </div>
-        @else
-            
-        @endif
-        <form action="{{ route('login') }}" method="POST">
-            @csrf
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" value="{{ old('email') }}" name="email" class="form-control">
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control">
-            </div>
-            <div class="mb-3 d-grid">
-                <button name="submit" type="submit" class="btn btn-primary">Login</button>
-            </div>
-        </form>
-    </div> 
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="input-group mb-3">
+                    <input
+                      type="text"
+                      class="form-control form-control-lg bg-light fs-6"
+                      placeholder="Email address"
+                      name="email"
+                    />
+                  </div>
+                  <div class="input-group mb-1">
+                    <input
+                      type="password"
+                      class="form-control form-control-lg bg-light fs-6"
+                      placeholder="Password"
+                      name="password"
+                    />
+                  </div>
+                  <div class="input-group mb-4 d-flex justify-content-between">
+                    <div class="form-check">
+                      <input
+                        type="checkbox"
+                        class="form-check-input"
+                        id="formCheck"
+                      />
+                      <label for="formCheck" class="form-check-label text-secondary"
+                        ><small>Remember Me</small></label
+                      >
+                    </div>
+                    <div class="forgot">
+                      <small><a href="#">Forgot Password?</a></small>
+                    </div>
+                  </div>
+                  <div class="input-group mb-3">
+                    <button class="btn btn-lg btn-primary w-100 fs-6" type="submit">Login</button>
+                  </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-</body>
+  </body>
 </html>
