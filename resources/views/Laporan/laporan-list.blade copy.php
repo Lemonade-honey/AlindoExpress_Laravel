0a8290a83a34239496fcbf89512b091a8a2fca6f
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-07-12 13:10:57 +0700

    Update UI

--- resources/views/Laporan/laporan-list.blade copy.php
+++ resources/views/Laporan/laporan-list.blade copy.php
@@ -6,16 +6,15 @@
     <title>Laporan</title>
 
     <link rel="stylesheet" href="assets/css/main/app.css" />
-    <link rel="stylesheet" href="assets/css/main/app-dark.css" />
 
     <link
       rel="shortcut icon"
-      href="assets/images/favicon.ico"
+      href="assets/images/logo/favicon.svg"
       type="image/x-icon"
     />
     <link
       rel="shortcut icon"
-      href="assets/images/favicon.ico"
+      href="assets/images/logo/favicon.png"
       type="image/png"
     />
   </head>
@@ -24,12 +23,42 @@
     <div id="app">
       @include('Includes._navbar')
       <div id="main" class="layout-navbar">
-        <header>
+        <header class="mb-3">
           <nav class="navbar navbar-expand navbar-light navbar-top">
             <div class="container-fluid">
               <a href="#" class="burger-btn d-block">
                 <i class="bi bi-justify fs-3"></i>
               </a>
+
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-lg-0"></ul>
+                <div class="dropdown">
+                  <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div class="user-menu d-flex">
+                      <div class="user-name text-end me-3">
+                        <h6 class="mb-0 text-gray-600">John Ducky</h6>
+                        <p class="mb-0 text-sm text-gray-600">Administrator</p>
+                      </div>
+                      <div class="user-img d-flex align-items-center">
+                        <div class="avatar avatar-md">
+                          <img src="assets/images/faces/1.jpg" />
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
             </div>
           </nav>
         </header>
@@ -41,17 +70,10 @@
                   <h3>Laporan</h3>
                 </div>
                 <div class="col-12 col-md-6 order-md-2 order-first">
-                  <nav
-                    aria-label="breadcrumb"
-                    class="breadcrumb-header float-start float-lg-end"
-                  >
+                  <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
-                      <li class="breadcrumb-item">
-                        <a href="/">Dashboard</a>
-                      </li>
-                      <li class="breadcrumb-item active" aria-current="page">
-                        Laporan
-                      </li>
+                      <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                      <li class="breadcrumb-item active" aria-current="page">Laporan</li>
                     </ol>
                   </nav>
                 </div>
@@ -93,9 +115,7 @@
                           </td>
                         </tr>
                         @empty
-                          <tr>
-                            <td colspan="3" class="text-center">Tidak ada Transaksi Pada Bulan Ini</td>
-                          </tr>
+                            
                         @endforelse
                       </tbody>
                     </table>
