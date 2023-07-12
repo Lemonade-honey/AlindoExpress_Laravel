43fd4353cf3beaa3bc161b04ebaf346c4022777a
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-07-12 11:46:06 +0700

    Perbaikan UI Paket

--- resources/views/Paket/paket-list.blade.php
+++ resources/views/Paket/paket-list.blade.php
@@ -24,45 +24,15 @@
     <div id="app">
       @include('Includes._navbar')
       <div id="main" class="layout-navbar">
-        <header class="mb-3">
-          <nav class="navbar navbar-expand navbar-light navbar-top">
-            <div class="container-fluid">
-              <a href="#" class="burger-btn d-block">
-                <i class="bi bi-justify fs-3"></i>
-              </a>
-
-              <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-lg-0"></ul>
-                <div class="dropdown">
-                  <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                    <div class="user-menu d-flex">
-                      <div class="user-name text-end me-3">
-                        <h6 class="mb-0 text-gray-600">John Ducky</h6>
-                        <p class="mb-0 text-sm text-gray-600">Administrator</p>
-                      </div>
-                      <div class="user-img d-flex align-items-center">
-                        <div class="avatar avatar-md">
-                          <img src="assets/images/faces/1.jpg" />
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                </div>
+        <header>
+            <nav class="navbar navbar-expand navbar-light navbar-top">
+              <div class="container-fluid">
+                <a href="#" class="burger-btn d-block">
+                  <i class="bi bi-justify fs-3"></i>
+                </a>
               </div>
-            </div>
-          </nav>
-        </header>
+            </nav>
+          </header>
         <div id="main-content">
           <div class="page-heading">
             <div class="page-title">
