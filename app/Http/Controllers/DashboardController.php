d5df3bb9c5bd93497b3fa4f3e83e11d637813ab4
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-07-28 22:52:24 +0700

    Ubah UI Dashboard dan gabungan backend

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Paket;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('Dashboard/dashboard');
-    }
-
-    public function admin(){
-        echo 'ini admin role';
+        $today = count(Paket::whereDate('created_at', Carbon::today())->get());
+        $mount = count(Paket::whereYear('created_at', '=', date('Y'))->whereMonth('created_at', '=', date('m'))->get());
+        $staf = count(DB::table('users')->get());
+        return view('Dashboard/dashboard', compact('today', 'mount', 'staf'));
     }
 
     public function logout(){
